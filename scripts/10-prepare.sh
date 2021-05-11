ce5f0a76e02c3997426ec5383287016575d0f5fe
Martin Eisenbarth <eyesee@foobarlab.net>
2021-05-11 14:45:54 +0200

    allow pre-downloaded distfiles

--- scripts/10-prepare.sh
+++ scripts/10-prepare.sh
@@ -5,6 +5,11 @@ if [ -z ${BUILD_RUN:-} ]; then
   exit 1
 fi
 
+# ---- copy pre-downloaded distfiles (if any)
+
+sf_vagrant="`sudo df | grep vagrant | tail -1 | awk '{ print $6 }'`"
+[[ -f "$sf_vagrant/distfiles" ]] && sudo rsync -urv $sf_vagrant/distfiles /var/cache/portage/
+
 # ---- import binary packages
 
 sf_vagrant="`sudo df | grep vagrant | tail -1 | awk '{ print $6 }'`"
