e01ad854aeceb8e672c70157198ddebae34b96e5
Martin Eisenbarth <eyesee@foobarlab.net>
2021-05-18 11:51:26 +0200

    testing

--- scripts/30-system-update.sh
+++ scripts/30-system-update.sh
@@ -18,6 +18,9 @@ sudo find /etc/ -name '._cfg*' -print -exec cat -n '{}' \;  # DEBUG: cat all con
 
 sudo etc-update --verbose --preen    # auto-merge trivial changes
 
+# DEBUG:
+eselect java-vm show
+
 user_id=$(id -u)    # FIX: because of "/etc/profile.d/java-config-2.sh: line 22: user_id: unbound variable" we try to set the variable here
 sudo env-update
 source /etc/profile
