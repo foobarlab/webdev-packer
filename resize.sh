3453771a0a598265e208fd101d12383299ebe9af
Martin Eisenbarth <eyesee@foobarlab.net>
2021-08-13 13:59:39 +0200

    todo

--- resize.sh
+++ resize.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 # vim: ts=2 sw=2 et
 
-if [ -z ${BUILD_RUN:-} ]; then
-  echo "This script can not be run directly! Aborting."
-  exit 1
-fi
+# FIXME add protection
+#if [ -z ${BUILD_RUN:-} ]; then
+#  echo "This script can not be run directly! Aborting."
+#  exit 1
+#fi
 
 echo "TODO resize with parted, then reboot"
