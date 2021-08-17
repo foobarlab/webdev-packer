6f81385c910be4e0b874b7d1e55bf00d3f14f840
Martin Eisenbarth <eyesee@foobarlab.net>
2021-08-17 12:07:51 +0200

    fix disk to 30 GB

--- config.sh
+++ config.sh
@@ -25,7 +25,7 @@ BUILD_GUEST_TYPE="Gentoo_64"
 # default memory/cpus used for final created box:
 BUILD_BOX_CPUS="2"
 BUILD_BOX_MEMORY="2048"
-BUILD_BOX_DISKSIZE="51200" # resize disk in MB, comment-in to disable
+BUILD_BOX_DISKSIZE="30720"   # resize disk to value in MB, comment-in to disable resizing
 
 # add a custom overlay?
 BUILD_CUSTOM_OVERLAY=false
